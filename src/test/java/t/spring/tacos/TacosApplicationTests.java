39377e598d4793fadaf05a5c25ccf4addc78edc4
Haja Andria <kantiiriley@gmail.com>
2022-02-10 10:02:23 -0500

    Starting tacos project

---
+++ src/test/java/t/spring/tacos/TacosApplicationTests.java
@@ -0,0 +1,13 @@
+package t.spring.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
